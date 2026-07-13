409bf062e79004fc9d74e91cd702f29281f3e468
gaetansanterne-glitch <gaetan.santerne@gmail.com>
2026-07-13 09:08:39 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,13 @@
+// ====================================
+// PRO CLIM
+// Application principale
+// Version 0.0.3
+// ====================================
 
+console.log("✅ Pro Clim démarré");
+
+document.addEventListener("DOMContentLoaded", () => {
+
+    console.log("Application chargée");
+
+});
